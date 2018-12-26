00ea62cdbeb7bd83e1d0bebf8d6627fe02dec5ce
Dameon Mendoza <dameonmendoza@gmail.com>
2018-12-26 08:17:44 -0700

    Updates

---
+++ precache-manifest.3efb3577f7baeba5264115ce40cf7949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spacedRepition-client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "048c0e9f8ababa187cd5",
+    "url": "/spacedRepition-client/static/js/runtime~main.048c0e9f.js"
+  },
+  {
+    "revision": "5aaece0fb07e57c3207d",
+    "url": "/spacedRepition-client/static/js/main.5aaece0f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/spacedRepition-client/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5aaece0fb07e57c3207d",
+    "url": "/spacedRepition-client/static/css/main.53f8a8f8.chunk.css"
+  },
+  {
+    "revision": "3693f03dfd9fd5446d3814d710854c3e",
+    "url": "/spacedRepition-client/index.html"
+  }
+];
