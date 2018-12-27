1cfb43060edfb6ca1554c05bff06d4b3ab014c66
Dameon Mendoza <dameonmendoza@gmail.com>
2018-12-27 10:32:32 -0700

    circleci testing

---
+++ src/__test__/headerBar.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+import React from 'react';
+import { shallow } from 'enzyme';
+import HeaderBar from '../components/header-bar';
+
+chai.use(chaiHttp);
+
+describe('<HeaderBar />', () => {       
+    it('Renders without crashing', () => {
+        shallow(<HeaderBar />);
+    });
+
+describe('<HeaderBar />', () => {       
+    it('Renders without correct title', () => {
+      const wrapper = shallow(<HeaderBar />)
+      const item = wrapper.find('[className="header-bar"]');
+      expect(item.text()).to.contain('SPANISH FLASH')
+    });
+  })
+});
---
+++ src/__test__/setup.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Spaced Rep app testing', () => {       
+    it('Should be true', () => {
+        expect(true).to.equal(true);
+    });  
+});
